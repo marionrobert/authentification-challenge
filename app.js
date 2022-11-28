0ac33efeac489ac51db8be28a841ac97fe3b7db2
Marion <mrobert148@gmail.com>
2022-11-28 10:46:34 +0100

    initial commit with comments and laucnh app on port 3000

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ const ejs = require("ejs");
 
 const app = express();
 
+// add public file as static resources
 app.use(express.static("public"));
+
+// set view engine to ejs
 app.set("view engine", "ejs");
+
+// set up bodyParser
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000.");
+})
